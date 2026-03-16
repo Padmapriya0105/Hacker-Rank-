08b972ac501084f30aa435a3dbdcff4cefcab8a5
Padmapriya0105 <pp179174_mca27@mepcoeng.ac.in>
2026-03-16 13:51:15 +0530

    Create Java stdin and stdout.java

---
+++ Java stdin and stdout.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        // Reading three integers from stdin
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        
+        scan.close();
+
+        // Printing each integer on a new line
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
